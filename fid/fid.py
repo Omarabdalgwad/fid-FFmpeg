8c2446354b91ed901a6acb73799a5f1645f841b7
Omar <ahlawyomar95@gmail.com>
2026-01-29 14:40:32 +0200

    closes #52

--- fid/fid.py
+++ fid/fid.py
@@ -1,11 +1,11 @@
 import typer
 from .welcome import welcome
-from .fid_jobs.extract.audio import audio_main
-from .fid_jobs.extract.frames import frames_main
-from .fid_jobs.audio.mute import mute_main
-from .fid_jobs.video.compressor import compress_main
-from .fid_jobs.video.gif import gif_main
-from .fid_jobs.info import info_main
+from .fid_tasks.extract.audio import audio_main
+from .fid_tasks.extract.frames import frames_main
+from .fid_tasks.audio.mute import mute_main
+from .fid_tasks.video.compressor import compress_main
+from .fid_tasks.video.gif import gif_main
+from .fid_tasks.info import info_main
 
 
 app= typer.Typer()
@@ -15,7 +15,7 @@ def start(ctx : typer.Context):
     if ctx.invoked_subcommand is None:
         welcome()
 
-        
+
 audio_main(app)
 compress_main(app)
 frames_main(app)
