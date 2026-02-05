3acdbb318b525d87de6177eb9a124e7f3572b8bb
Omar <ahlawyomar95@gmail.com>
2026-02-05 03:10:00 +0200

    denoise

---
+++ src/fid/tasks/audio/denoise.py
@@ -0,0 +1,13 @@
+import typer
+import subprocess
+from pathlib import Path
+from ...initial_files.error_handling import ffmpeg , ckvideo
+
+def denoise(cPath: Path):
+    ffmpeg()
+    ckvideo(cPath)
+    denoise_out= cPath.with_stem(f"{cPath.stem}_denoise")
+    subprocess.run({"ffmpeg","-i",str(cPath),"-af","afftdn",str(denoise_out)},check=True)
+
+def denoise_main(app: typer.Typer):
+    app.command()(denoise)
