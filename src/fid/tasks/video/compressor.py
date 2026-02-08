4fde07d46b9545aecfdb2db48743f0ff636e1b03
Omar <ahlawyomar95@gmail.com>
2026-02-08 05:01:09 +0200

    add

--- src/fid/tasks/video/compressor.py
+++ src/fid/tasks/video/compressor.py
@@ -4,12 +4,12 @@ from pathlib import Path
 from ...initial_files.error_handling import ffmpeg , ckvideo
 
 
-def compress(cPath: Path, crf: int=28):
+def compress(cPath: Path, crf: int,preset: str, audio_bitrate: str):
     ffmpeg()
     ckvideo(cPath)
-    compress_out= cPath.with_stem(f"{cPath.stem}_compressed").with_suffix(".mkv")
+    compress_out= cPath.with_stem(f"{cPath.stem}_compressed")
     subprocess.run(
-            ["ffmpeg", "-i", str(cPath),"-c:v", "libx264", "-crf", str(crf), "-preset","medium","-c:a","aac","-b:a","96k","-y",str(compress_out),]
-            , check=True)
+            ["ffmpeg", "-i", str(cPath),"-c:v", "libx264", "-crf", str(crf), "-preset",preset,"-c:a","aac","-b:a",audio_bitrate,"-y",str(compress_out),]
+            , check=True,stdout=subprocess.DEVNULL)
 def compress_main(app: typer.Typer):
     app.command()(compress)
