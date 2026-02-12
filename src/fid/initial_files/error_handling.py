3b927d88ca6c2f36949726491b16ac337bb9a481
Omar <ahlawyomar95@gmail.com>
2026-02-12 04:30:53 +0200

    ffmpeg installation #137

--- src/fid/initial_files/error_handling.py
+++ src/fid/initial_files/error_handling.py
@@ -1,32 +1,44 @@
-import requests
+import requests, zipfile, platform , rich
 from pathlib import Path
 from tqdm import tqdm
-import platform
-import typer
+from rich.console import Console
+from time import sleep
 
 def ffmpeg():
-    folder = Path("ffmpeg")
-    folder.mkdir(exist_ok=True) 
-    if platform.system() == "Windows":
-       url = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials/ffmpeg.exe"
-       exe = "ffmpeg.exe"
 
-    exe_path= folder / exe
-    if exe_path.exists(): 
-        print("FFmpeg is installed")
-        return exe_path
+    if platform.system()!= "Windows": 
+       print("Windows only is supported for ffmpeg installation")
+       print(" plz download ffmpeg from https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip")
 
-    print("Downloading FFmpeg....\n")
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        total= int(r.headers.get("content-length", 0))
-        with open(exe_path,"wb") as f,tqdm(total=total,unit="B",unit_scale=True) as bar:
-            for chunk in r.iter_content(1024):
+    FFmpeg_installation= Path.home()/".fid-ffmpeg"
+    FFmpeg_installation.mkdir(exist_ok=True)
+    zip=FFmpeg_installation/"ffmpeg.zip"
+    exe=FFmpeg_installation/"ffmpeg.exe"
+    console=Console()
+    with console.status("checking for ffmpeg....") as status:
+        sleep(2)
+        if exe.exists():
+            print("ffmpeg already exists")
+            return
+        print("ffmpeg not found, downloading it ...")
+    r =requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip",stream=True)
+    total = int(r.headers.get("content-length"))
+    with open(zip,"wb")as f:
+         with tqdm(total=total,unit="B",unit_scale=True,colour="green", bar_format="{l_bar}{bar} | {n_fmt}/{total_fmt}" ) as bar:
+            for chunk in r.iter_content(1024*1024):
                 f.write(chunk)
                 bar.update(len(chunk))
-
+    with console.status("extracting....") as status:
+        sleep(1.5)
+        with zipfile.ZipFile(zip)as z:
+            for name in z.namelist():
+                if name.endswith("ffmpeg.exe"):
+                   extract= z.extract(name , FFmpeg_installation)
+                   Path(extract).rename(exe)
+                   print("ffmpeg installed .")
+                   break
 
 def ckvideo(cPath):
-    if not cPath.exists():  # لو الفيديو مش موجود يخرج
-        print("this video doesn't exist")
-        raise typer.Exit()
+    if not cPath.exists() or not cPath.is_file() or cPath.suffix not in [".mp4",".avi",".mkv",".mov",".flv",".wmv",".webm"]:  
+        print("incorrect video path or unsupported video fromat")
+        exit()
